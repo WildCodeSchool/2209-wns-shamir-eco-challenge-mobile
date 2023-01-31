f464382f74a70ecf5f97e0860362933de15b3ba6
nicovray <auvnic@gmail.com>
2023-01-31 11:47:57 +0100

    amélioration du design de l'accueil avec de sfeuilles plus grandes et et des backgrounds colorés transparents

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -7,41 +7,45 @@ export default function Home() {
   return (
     <ScrollView>
       <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={require('../assets/logo.png')}
-      />
+        <Image
+          style={styles.logo}
+          source={require('../assets/logo.png')}
+        />
 
-      <Text style={styles.intro}>
-        Envie de lancer un challenge à vos amis ? "Eco-Challenges" vous permet de
-        créer des challenges personnalisés constitués d'éco-gestes à réaliser
-        pour vous informer et vous sensibiliser sur votre impact énergétique au
-        quotidien !
-      </Text>
+        <Text style={styles.intro}>
+          Envie de lancer un challenge à vos amis ? "Eco-Challenges" vous permet de
+          créer des challenges personnalisés constitués d'éco-gestes à réaliser
+          pour vous informer et vous sensibiliser sur votre impact énergétique au
+          quotidien !
+        </Text>
 
-      <Text style={styles.login}><Ionicons name={"leaf-outline"} color="#357452ff"/> Identifiant :</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="ton email"
-      />
-      
-      <Text style={styles.login}><Ionicons name={"leaf-outline"} color="#357452ff"/> Mot de passe :</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="ton mot de passe"
-      />
+        <View style={styles.backgroundLogin}>
+          <Text style={styles.loginText}><Ionicons name={"leaf-outline"} color="#357452ff" size={20}/> Identifiant :</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="ton email"
+          />
 
-      <Button
-        title="SE CONNECTER"
-        color="#357452ff"
-      />
+          <Text style={styles.loginText}><Ionicons name={"leaf-outline"} color="#357452ff" size={20}/> Mot de passe :</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="ton mot de passe"
+          />
+        
+          <Button
+            title="SE CONNECTER"
+            color="#357452ff"
+          />
+        </View>
 
-      <Text style={styles.register}>Nouveau challenger ?</Text>
-
-      <Button 
-        title="S'ENREGISTRER"
-        color="#357452ff"
-      />
+        <View style={styles.backgroundRegister}>
+          <Text style={styles.registerText}>Nouveau challenger ?</Text>
+        
+          <Button 
+            title="S'ENREGISTRER"
+            color="#357452ff"
+          />
+        </View>
       </View>
     </ScrollView>
   );
@@ -68,29 +72,44 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: "open-sans",
     textAlign: "center",
-    marginTop: 20,
-    marginBottom: 50,
-    
+    marginTop: 20,    
   },
-  login: {
+ backgroundLogin: {
+    backgroundColor: "rgba(53, 116, 82, 0.05)",
+    width: 350,
+    borderRadius: 10,
+    alignItems: "center",
+    padding: 20,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  loginText: {
     fontSize: 16,
     fontFamily: "open-sans",
   },
-  register: {
+  backgroundRegister: {
+    backgroundColor: "rgba(53, 116, 82, 0.05)",
+    width: 350,
+    borderRadius: 10,
+    alignItems: "center",
+    padding: 20,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  registerText: {
     fontSize: 16,
     fontWeight: "bold",
     fontFamily: "open-sans",
-    marginTop: 50,
-    marginBottom: 30,
+    marginBottom: 20,
   },
   input: {
     height: 40,
-    marginBottom: 30,
     borderRightWidth: 0,
     borderLeftWidth: 0,
     borderTopWidth: 0,
     borderBottomWidth: 2,
     borderBottomColor: "#357452ff",
     padding: 10,
+    marginBottom: 20,
   },
 });
